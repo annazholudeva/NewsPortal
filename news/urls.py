c40eca87814cc4469952db63f27c7ee919b9f135
AnnaZholudeva <59976643+annazholudeva@users.noreply.github.com>
2023-08-10 22:28:28 +0300

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import NewsList, NewsDetail, NewsCreate, NewsUpdate, NewsDelete
+from .views import NewsList, NewsDetail, NewsCreate, NewsUpdate, NewsDelete, NewsSearch
 
 urlpatterns = [
-   path('', NewsList.as_view()),
+   path('', NewsList.as_view(), name='allnews'),
+   path('create/', NewsCreate.as_view(), name='news_create'),
+   path('search/', NewsSearch.as_view(), name='news_search'),
    path('<int:pk>', NewsDetail.as_view(), name='news'),
-   path('create/', NewsCreate.as_view()),
-   path('<int:pk>/update/', NewsUpdate.as_view()),
-   path('<int:pk>/delete/', NewsDelete.as_view()),
+   path('<int:pk>/update/', NewsUpdate.as_view(), name='news_edit'),
+   path('<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
 ]
